658f842e535503cdb23d9dc5ebb74fae57e465bc
Kandaurov-Aleksandr <kandayrov1990@gmail.com>
2021-04-30 08:38:54 +0300

    add files in hw-04 branch

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
-
-const contactsRouter = require('./routes/api/contacts')
-
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 const app = express()
-
+const contactsRouter = require('./routes/api/contactsRoutes')
+const authRouter = require('./contactAuth/router')
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+dotenv.config()
+require('./config/passport')
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+const runServer = async () => {
+  try {
+    await mongoose.connect(process.env.URI_DB, {
+      promiseLibrary: global.Promise,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+    console.log('Database connection is successful')
 
-app.use('/api/contacts', contactsRouter)
+    app.use(logger(formatsLogger))
+    app.use(cors())
+    app.use(express.json())
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+    app.use('/auth', authRouter)
+    app.use('/api/contacts', contactsRouter)
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+    app.use((req, res) => {
+      res.status(404).json({ message: 'Contact is not found' })
+    })
+
+    app.use((err, req, res, next) => {
+      res.status(500).json({ message: err.message })
+    })
+  } catch (err) {
+    process.exit(1)
+  }
+}
+
+runServer()
 
 module.exports = app
